043688e4cc1976befc5079baeed1cf1d4e7576e2
Kolka <lamn4t@yandex.ru>
2020-09-14 20:26:56 +0300

    Первое решение

---
+++ save_photo.py
@@ -0,0 +1,78 @@
+import requests, json, time, os
+from operator import itemgetter
+from tqdm import tqdm
+import shutil
+
+def get_photo_dict(ident,quantity):
+    api = requests.get(
+    'https://api.vk.com/method/photos.get',
+    params={
+        'access_token': '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008',
+        'v': 5.122,
+        'owner_id': ident,
+        'album_id':'profile',
+        'count':quantity,
+        'photo_sizes': 1,
+        'extended': 1
+    })
+    return json.loads(api.text)
+
+def get_foto(ident,quantity):
+    data = get_photo_dict(ident,quantity)
+    os.mkdir('photos')
+    info_photos = []
+    print("Скачиваем фото с vk.com")
+    for files in tqdm(data['response']['items']):
+        sizes_sort = files['sizes']
+        sorted_dict = sorted(sizes_sort, key=itemgetter('width','height'))
+        file_url = sorted_dict[-1]['url']
+        filename = str(files['likes']['count']) + '.jpg'
+        size = str(sorted_dict[-1]['height']) + 'x' + str(sorted_dict[-1]['width']) 
+        info_dict = {}
+        info_dict.setdefault('filename',filename)
+        info_dict.setdefault('size',size)
+        info_photos.append(info_dict)
+        time.sleep(0.1)
+        api = requests.get(file_url)
+
+        with open('photos/%s' % filename, 'wb') as file:
+            file.write(api.content)
+    
+    json_foto = {}
+    json_foto.setdefault('photo', info_photos)
+    with open('photos.json', 'w') as f:
+        json.dump(json_foto, f,sort_keys=True, indent=2)
+
+def directory(token):
+    url = 'https://cloud-api.yandex.net:443/v1/disk/resources'
+    params = {'path': 'photos'}
+    headers = {'Authorization': token}
+    resp = requests.put(url, params=params, headers=headers)
+    upload(token)
+    shutil.rmtree('photos')
+    os.mkdir('photos')
+    os.rmdir('photos')
+
+def upload(token):
+    files = os.listdir('photos/')
+    print('Загружаем фото на Яндекс Диск')
+    for n in tqdm(files):
+        directory = 'photos/' + n
+        url = 'https://cloud-api.yandex.net:443/v1/disk/resources/upload'
+        params = {'path': 'photos/' + n}
+        headers = {'Authorization': token}
+        resp = requests.get(url, params=params, headers=headers)
+        resp2 = resp.json()
+        url2 = resp2['href']
+        with open(directory, 'rb') as f:
+            resp = requests.post(url2, files={"file": f})
+
+def main():
+    ident = int(input('Введите номер id: '))
+    quantity = int(input('Введите количество фото: '))
+    token_ya = input('Введите токен Яндекс Диск: ')
+    get_foto(ident, quantity)
+    directory(token_ya)
+
+if __name__ == '__main__':
+    main()
